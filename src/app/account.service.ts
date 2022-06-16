22a14acf82ca66dee31ad3625c2e753de686fd4e
Aaron <aaronjerez1@gmail.com>
2022-06-16 11:43:09 -0400

    catch up

---
+++ src/app/account.service.ts
@@ -0,0 +1,35 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+@Injectable()
+export class AccountService {
+    accounts = [
+        {
+          name: 'Master Account',
+          status: 'active'
+        },
+        {
+          name: 'Testaccount',
+          status: 'inactive'
+        },
+        {
+          name: 'Hidden Account',
+          status: 'unknown'
+        }
+    ];
+
+    constructor(private loggingService: LoggingService){
+
+    }
+
+    statusUpdated = new EventEmitter<string>();
+
+    addAccount(name: string, status: string){
+        this.accounts.push({name: name, status: status}); //this is how you create an object on the fly with those fields.
+
+    }
+
+    updateStatus(id:number, status: string)
+    {
+        this.accounts[id].status = status;
+    }
+}
